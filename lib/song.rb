8d1cb1b429e1e5a17ccb89b6d14529ab25c700be
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-10-07 03:41:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,9 +25,8 @@ class Song
   end
   
   def self.create_by_name(name)
-    self.new_by_name(name)
-    #song = self.new
-    #song.name = name
+    song = self.new
+    song.name = name
     song.save
     song
   end
